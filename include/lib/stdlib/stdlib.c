e050e50bdbc38df9997416cf27fc25b97f97778f
v4rsh1th <varshithjay6@gmail.com>
2021-05-29 22:04:53 +0530

    Fix everything and add IDT.

---
+++ include/lib/stdlib/stdlib.c
@@ -0,0 +1,18 @@
+#include "../stdlib.h"
+#include <cze/print.h>
+
+u8 intlen(u64 num) {
+    // Length starts at one.
+    u8 length = 1;
+    // Keep dividing by ten (base) to reach length.
+    while(num > 9) {
+        length++;
+        num /= 10;
+    }
+    return length;
+}
+
+void* malloc(u32 size) {
+    // Placeholder.
+    return (void*)(0x00000f);
+}
