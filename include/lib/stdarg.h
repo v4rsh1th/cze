de01266b73b498b23ae6f8e79c99b39ecea83a64
v4rsh1th <varshithjay6@gmail.com>
2021-05-30 22:43:53 +0530

    Fix everything and add IDT.

---
+++ include/lib/stdarg.h
@@ -0,0 +1,12 @@
+#ifndef STDARG_H
+#define STDARG_H
+#include <util/types.h>
+
+typedef struct {
+  void *cur;
+} va_list;
+
+#define va_start(list, ref) list.cur = &ref + sizeof(ref);
+#define va_arg(list, type) *(type*)list.cur; list.cur += sizeof(type);
+
+#endif
