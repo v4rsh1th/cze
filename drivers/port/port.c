948bc508d510b1690cb654565653f5dfe82b15b9
v4rsh1th <varshithjay6@gmail.com>
2021-05-31 18:26:53 +0530

    Rookie mistake v0.0.3

--- drivers/port/port.c
+++ drivers/port/port.c
@@ -1,9 +1,17 @@
-#ifndef PORT_H
-#define PORT_H
-#include <util/types.h>
+#include "port.h"
 
-u8 portByteIn(u16 port);
-void portByteOut(u16 port, u8 value);
-u16 portWordIn(u16 port);
+u8 portByteIn(u16 port) {
+  u8 output;
+  __asm__("in %%dx, %%al" : "=a" (output) : "d" (port));
+  return output;
+}
 
-#endif
+void portByteOut(u16 port, u8 value) {
+  __asm__("out %%al, %%dx" : : "a" (value), "d" (port));
+}
+
+unsigned short portWordIn(u16 port) {
+  u16 output;
+  __asm__("in %%dx, %%ax" : "=a" (output) : "d" (port));
+  return output;
+}
