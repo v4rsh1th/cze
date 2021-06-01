454ea69f1a705bb7c7d52698d2e9d5fa72ea1c83
v4rsh1th <varshithjay6@gmail.com>
2021-06-01 21:16:25 +0530

    Adding ability to interpolate strings, remove testing IDT code.

---
+++ include/lib/string.h
@@ -0,0 +1,13 @@
+#ifndef STRING_H
+#define STRING_H
+#include <util/types.h>
+
+u32 strlen(s8 *str);
+
+s8 strcpy(s8 *str, const s8 *copy);
+
+s8 strcat(s8 *str, const s8 *add);
+
+void memset(void *pointer, u32 item, u64 length);
+
+#endif
