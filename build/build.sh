ece8eaf4f5169c4121ef080d0f3d8cd63bee938c
v4rsh1th <varshithjay6@gmail.com>
2021-06-06 13:37:20 +0530

    Rookie mistake v0.0.4

--- build/build.sh
+++ build/build.sh
@@ -3,27 +3,26 @@ OBJECTFILES="wrapper/wrapper.o"
 
 # Utility to compile a file
 function compile {
-  FILE=$1
-  EXTENSION=${FILE: -2}
-  NEWFILE=${FILE:0:${#FILE}-2 }.o
+    FILE=$1
+    EXTENSION=${FILE: -2}
+    NEWFILE=${FILE:0:${#FILE}-2 }.o
 
-  if [ $EXTENSION == ".c" ]
-  then
-    x86_64-elf-gcc -Wall -ffreestanding -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -c $FILE -o $NEWFILE -I include
-  elif [ $EXTENSION == "sm" ]
-  then
-    /usr/local/bin/nasm -f elf64 $FILE -o $NEWFILE
-  fi
-
-  OBJECTFILES+=" $NEWFILE"
+    if [ $EXTENSION == ".c" ]
+    then
+        x86_64-elf-gcc -Wall -ffreestanding -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -c $FILE -o $NEWFILE -I include
+    elif [ $EXTENSION == "sm" ]
+    then
+        /usr/local/bin/nasm -f elf64 $FILE -o $NEWFILE
+    fi
+    OBJECTFILES+=" $NEWFILE"
 }
 
 # Utility to compile all C and Assembly files in a directory
 function compileAll {
   ALLFILES=$(find $1 -type f -name '*.c' -o -name '*.asm')
-  for file in $ALLFILES ; do
-      compile $file
-  done
+      for file in $ALLFILES ; do
+        compile $file
+      done
 }
 
 # Compile Bootloader
@@ -34,31 +33,31 @@ function compileAll {
 compileAll drivers
 compileAll include/util
 compileAll include/lib
-compileAll include/arc
+compileAll include/cze
 compileAll kernel/cpu
 compileAll kernel/process
 compile kernel/kernel.c
 x86_64-elf-ld -o kernel/kernel.bin -nostdlib -T build/build.lds $OBJECTFILES
 
 # Compile into one file
-cat boot/boot.bin kernel/kernel.bin > dist/arc
+cat boot/boot.bin kernel/kernel.bin > dist/cze
 
 # Cleanup old files
-rm -f dist/arc.iso
+rm -f dist/cze.iso
 
 # Put onto floppy image
-dd if=/dev/zero of=dist/arc.img bs=1024 count=1440
-dd if=./dist/arc of=dist/arc.img seek=0 conv=notrunc
+dd if=/dev/zero of=dist/cze.img bs=1024 count=1440
+dd if=./dist/cze of=dist/cze.img seek=0 conv=notrunc
 
 # Remove binary file
-rm -rf dist/arc
+rm -rf dist/cze
 
 # Create temporary iso directory and move image to it
 mkdir dist/iso
-mv dist/arc.img dist/iso
+mv dist/cze.img dist/iso
 
 # Create ISO
-mkisofs -quiet -V 'ARC' -o dist/arc.iso -b arc.img -hide arc.img dist/iso
+mkisofs -quiet -V 'ARC' -o dist/cze.iso -b cze.img -hide cze.img dist/iso
 
 # Remove temporary iso directory
 rm -rf dist/iso
